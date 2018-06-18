cfaa060eec3f054616ee23742f4b41055a14eb74
Gokulraja13 <gokulraja@tuta.io>
2018-06-18 17:24:03 +0530

    new solution

--- solution/BinaryGap.py
+++ solution/BinaryGap.py
@@ -1,2 +1,14 @@
-def BinaryGap(Number):
-  pass
+def solution(Number):
+    Binary_string = '{0:b}'.format(Number)
+    print(Binary_string)
+    count = 0
+    Global_count = 0
+    length = len(Binary_string)
+    for i in range(length):
+        if (Binary_string[i] == '0'):
+            count = count + 1
+        else:
+            if(count > Global_count):
+                Global_count = count
+            count = 0
+    print(Global_count)
